52a6936dca45f187631e9f4ae21acc1eab4ce4f1
John McCombs <internal@coredevelop.com>
2021-10-28 00:45:59 +0800

    Added a patch for Aheadworks Onestepcheckout payment method

---
+++ view/frontend/web/js/view/actions-toolbar/renderer/creditkey-gateway.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright 2019 aheadWorks. All rights reserved.\nSee LICENSE.txt for license details.
+ */
+
+define([
+    'Aheadworks_OneStepCheckout/js/view/actions-toolbar/renderer/default'
+], function (Component) {
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            template: 'CreditKey_B2BGateway/actions-toolbar/renderer/creditkey-gateway'
+        },
+
+        /**
+         * Redirect to CreditKey
+         */
+        redirectToPayment: function () {
+            var self = this;
+
+            this._beforeAction().done(function () {
+                self._getMethodRenderComponent().redirectToPayment();
+            });
+        }
+    });
+});
